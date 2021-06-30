030c3ed3a80fa268a449b89af9561194193962d6
Kyle Nedbal <kylenedbal@quickenloans.com>
2021-06-30 09:19:30 -0400

    help desk capstone project

---
+++ HelpDesk/ClientApp/src/app/ticket-details/ticket-details.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-ticket-details',
+    templateUrl: './ticket-details.component.html',
+    styleUrls: ['./ticket-details.component.css']
+})
+/** TicketDetails component*/
+export class TicketDetailsComponent {
+    /** TicketDetails ctor */
+    constructor() {
+
+    }
+}
---
+++ HelpDesk/Controllers/DAL.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Controllers
+{
+    public class DAL : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
